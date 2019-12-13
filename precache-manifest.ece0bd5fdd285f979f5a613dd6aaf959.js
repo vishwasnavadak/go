838f7aacaf43e80d918243555fa1923e5963d669
Vishwas Navada <navadavishwas@hotmail.com>
2019-12-13 23:44:27 +0530

    Updates

---
+++ precache-manifest.ece0bd5fdd285f979f5a613dd6aaf959.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d68c8b77bc682dd05e68fda947e343",
+    "url": "/go/index.html"
+  },
+  {
+    "revision": "0897652964ce3bcfa03d",
+    "url": "/go/static/js/2.0a424ee8.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/go/static/js/2.0a424ee8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c03efa114d655000ce25",
+    "url": "/go/static/js/main.a3297b48.chunk.js"
+  },
+  {
+    "revision": "4e4033ecd7c8ef9b610d",
+    "url": "/go/static/js/runtime-main.7cb1fddc.js"
+  }
+]);
